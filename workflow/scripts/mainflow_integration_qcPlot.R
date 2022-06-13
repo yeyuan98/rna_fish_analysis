c0a8a3e9fb329c30313b1903f8866a8a9fb751d2
yeyuan98 <yuanye1998@gmail.com>
2022-06-13 13:51:34 -0400

    Developing QC Plots
    Working on qcplot.R

--- workflow/scripts/mainflow_integration_qcPlot.R
+++ workflow/scripts/mainflow_integration_qcPlot.R
@@ -55,7 +55,7 @@ switch(plot.type,
            mutate(z.in.physical = # '+' direction increase with z_in_pix; otherwise decrease; both take the same range.
                     ifelse(z_direction == "+", (z_in_pix -1), (z_pixel_num - z_in_pix)) * physicalSizeZ) %>%
            ggplot(aes(x=z.in.physical, y=integratedIntensity))+
-           geom_point()+
+           geom_point(alpha=0.3)+
            geom_smooth()+
            scale_y_log10(expand = c(0.05, 0.05))+
            xlab("Z Position per dot (physical unit)")+
