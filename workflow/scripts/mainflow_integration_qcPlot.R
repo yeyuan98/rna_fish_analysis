97a22309dbef68b8969a5584b0cbbbe5125a8741
yeyuan98 <yuanye1998@gmail.com>
2022-06-13 23:03:10 -0400

    improving workingDist QC

--- workflow/scripts/mainflow_integration_qcPlot.R
+++ workflow/scripts/mainflow_integration_qcPlot.R
@@ -22,6 +22,7 @@ dots <- dots.full  # QC Plot needs raw dots.csv integrated data
 workingdist.significance.filter <- function(dots.df, signif.threshold = 0.01){
   #  Performs lm fit for each image and returns only images that hold significant slope fit.
   dots.df %>%
+    group_by(sample, image) %>%
     summarize(slope.signif = summary(lm(y~x, data.frame(y=integratedIntensity, x=z.in.physical)))$coefficients[2,4]) %>%
     ungroup() %>%
     inner_join(dots.df, by = c("sample", "image")) %>%
