a4761a88215c6c5adf000c3d055b9a6c18a048e1
yeyuan98 <yuanye1998@gmail.com>
2022-06-13 23:41:10 -0400

    This might be the best solution with facet_wrap

--- workflow/scripts/mainflow_integration_qcPlot.R
+++ workflow/scripts/mainflow_integration_qcPlot.R
@@ -71,8 +71,9 @@ switch(plot.type,
            scale_y_log10(expand = c(0.05, 0.05))+
            geom_smooth(method="lm", formula= y~x, se=F)+
            stat_fit_glance(method = 'lm', method.args = list(formula = y~x), geom = 'text',
-                       aes(label = paste0("P-value = ", signif(..p.value.., digits = 4))),
-                       label.x.npc = 'right', label.y.npc = 0.35, size = 3)+
+                       aes(label = paste0("P-value = ", signif(..p.value.., digits = 3),
+                                          "\n","Adj. Rsq = ", signif(..adj.r.squared.., digits = 3))),
+                       label.x = 0.8, label.y = 0.8, size = 3)+
            xlab("Z Position per dot (physical unit)")+
            ylab("Integrated intensity per dot (arbitrary unit)")+
            facet_wrap(vars(unique.label))+
