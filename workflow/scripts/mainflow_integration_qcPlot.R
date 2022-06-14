36a03311585bb3c31b6eecc4d7788bd32579fa85
yeyuan98 <yuanye1998@gmail.com>
2022-06-13 23:06:03 -0400

    improving workingDist QC

--- workflow/scripts/mainflow_integration_qcPlot.R
+++ workflow/scripts/mainflow_integration_qcPlot.R
@@ -91,10 +91,11 @@ switch(plot.type,
 
 
 out.path <- snakemake@output[["plot"]]
+
 ggsave(filename = basename(out.path),
        path = dirname(out.path),
        dpi = "retina",
-       width = plot.width.in,
+       width = ifelse(plot.type == "workingDist", 2, 1) * plot.width.in,
        height = plot.height.in,
        units = "in")
 
