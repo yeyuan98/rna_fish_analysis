6bce2af529db8493fc3bfb373657d3ce9cac0b39
yeyuan98 <yuanye1998@gmail.com>
2022-06-24 14:29:04 -0400

    Improving countPlot

--- workflow/scripts/mainflow_integration_countPlot.R
+++ workflow/scripts/mainflow_integration_countPlot.R
@@ -17,7 +17,7 @@ source("workflow/scripts/mainflow_integration_plot_loader.R")
 base.countPlot <-
   ggplot(data = NULL, aes(x = group, y = dots.per.cell))+
   geom_point()+
-  stat_summary(fun.data = mean_cl_normal, #fun.args = list(mult=1),
+  stat_summary(fun.data = "mean_cl_normal", fun.args = list(mult=1),
                geom="errorbar", color="red", width=0.2)+  # This calculates s.e.m.
   stat_summary(fun = mean, geom="point", color="red", size=2, alpha=0.5)+  # This calculates mean.
   scale_y_continuous(expand = c(0.05,0.05), limits = c(plot.ymin, plot.ymax))+
