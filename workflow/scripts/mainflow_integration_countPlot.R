9ba7ca4f711b397f991af32629862681f283e012
yeyuan98 <yuanye1998@gmail.com>
2022-06-24 15:15:25 -0400

    Debugging countPlot

--- workflow/scripts/mainflow_integration_countPlot.R
+++ workflow/scripts/mainflow_integration_countPlot.R
@@ -12,12 +12,18 @@ library(tidyverse)
 
 source("workflow/scripts/mainflow_integration_plot_loader.R")
 
+# A custom function for error bar calculation
+sem.error <- function(x){
+  sem <- sd(x) / sqrt(length(x))
+  m <- mean(x)
+  data.frame(ymin=m-sem, y=m, ymax=m+sem)
+}
 
 # Define the base plot without data
 base.countPlot <-
   ggplot(data = NULL, aes(x = group, y = dots.per.cell))+
   geom_point()+
-  stat_summary(fun.data = "mean_cl_normal", fun.args = list(mult=1),
+  stat_summary(fun.data = sem.error, fun.args = list(mult=1),
                geom="errorbar", color="red", width=0.2)+  # This calculates s.e.m.
   stat_summary(fun = mean, geom="point", color="red", size=2, alpha=0.5)+  # This calculates mean.
   scale_y_continuous(expand = c(0.05,0.05), limits = c(plot.ymin, plot.ymax))+
@@ -34,7 +40,6 @@ switch(plot.type,
          message(paste("Generating merged count plot for", probe))
          plot.data <- dots %>% mutate(dots.per.cell = dot.count / num.cells)
          plot.countPlot <- base.countPlot %+% plot.data
-         plot.countPlot
        },
        batch.qc={
          plot.data <- dots %>% mutate(dots.per.cell = dot.count / num.cells)
